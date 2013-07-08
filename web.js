03806abde007368b31846af70a4b285f4a6b69e4
Dimitrios Paraschas <paraschas@gmail.com>
2013-07-08 08:26:13 +0000

    added documentation

--- web.js
+++ web.js
@@ -5,8 +5,11 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   // return the file contents as a string
+  // http://nodejs.org/api/fs.html#fs_fs_readfilesync_filename_options
   //response.send(fs.readFileSync('./index.html', 'utf8'));
   // return the file contents as a buffer, convert it to a string
+  // http://nodejs.org/api/buffer.html#buffer_buf_tostring_encoding_start_end
+  // http://docs.nodejitsu.com/articles/advanced/buffers/how-to-use-buffers
   response.send(fs.readFileSync('./index.html').toString());
 });
 
