d9339495abd5bb14ce787ec991de32a8ac8f391d
Dimitrios Paraschas <paraschas@gmail.com>
2013-07-09 17:24:05 +0000

    URL checking option implemented

--- grader.js
+++ grader.js
@@ -25,12 +25,14 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var restler = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://www.google.com/ncr";
 
 var assertFileExists = function(infile) {
   var instr = infile.toString();
-  if(!fs.existsSync(instr)) {
+  if (!fs.existsSync(instr)) {
     console.log("%s does not exist. Exiting.", instr);
     process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
   }
@@ -41,6 +43,10 @@ var cheerioHtmlFile = function(htmlfile) {
   return cheerio.load(fs.readFileSync(htmlfile));
 };
 
+var cheerioHtmlBuffer = function(htmlbuffer) {
+  return cheerio.load(htmlbuffer);
+};
+
 var loadChecks = function(checksfile) {
   return JSON.parse(fs.readFileSync(checksfile));
 };
@@ -49,7 +55,18 @@ var checkHtmlFile = function(htmlfile, checksfile) {
   $ = cheerioHtmlFile(htmlfile);
   var checks = loadChecks(checksfile).sort();
   var out = {};
-  for(var ii in checks) {
+  for (var ii in checks) {
+    var present = $(checks[ii]).length > 0;
+    out[checks[ii]] = present;
+  }
+  return out;
+};
+
+var checkHtmlBuffer = function(htmlbuffer, checksfile) {
+  $ = cheerioHtmlBuffer(htmlbuffer);
+  var checks = loadChecks(checksfile).sort();
+  var out = {};
+  for (var ii in checks) {
     var present = $(checks[ii]).length > 0;
     out[checks[ii]] = present;
   }
@@ -66,10 +83,27 @@ if(require.main == module) {
   program
     .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
     .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+    .option('-u, --url <url>', 'HTTP/HTTPS link', URL_DEFAULT)
     .parse(process.argv);
-  var checkJson = checkHtmlFile(program.file, program.checks);
-  var outJson = JSON.stringify(checkJson, null, 4);
-  console.log(outJson);
+  if (program.file !== HTMLFILE_DEFAULT) {
+    var checkJson = checkHtmlFile(program.file, program.checks);
+    var outJson = JSON.stringify(checkJson, null, 4);
+    console.log(outJson);
+  } else {
+    restler.get(program.url).on('complete', function(result) {
+      if (result instanceof Error) {
+        console.log('Error: ' + result.message);
+        this.retry(4000);
+      } else {
+        // DEBUG
+        //console.log(result);
+
+        var checkJson = checkHtmlBuffer(result, program.checks);
+        var outJson = JSON.stringify(checkJson, null, 4);
+        console.log(outJson);
+      }
+    });
+  }
 } else {
   exports.checkHtmlFile = checkHtmlFile;
 }
