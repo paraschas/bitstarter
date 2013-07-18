f8424cf0a933b6805df8f791b6965ee82b170716
Dimitrios Paraschas <paraschas@gmail.com>
2013-07-18 15:29:53 +0300

    reversed static files serving by node

--- web.js
+++ web.js
@@ -3,18 +3,18 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-//app.get('/', function(request, response) {
-//  // return the file contents as a string
-//  // http://nodejs.org/api/fs.html#fs_fs_readfilesync_filename_options
-//  //response.send(fs.readFileSync('./index.html', 'utf8'));
-//  // return the file contents as a buffer, convert it to a string
-//  // http://nodejs.org/api/buffer.html#buffer_buf_tostring_encoding_start_end
-//  // http://docs.nodejitsu.com/articles/advanced/buffers/how-to-use-buffers
-//  response.send(fs.readFileSync('./index.html').toString());
-//});
+app.get('/', function(request, response) {
+  // return the file contents as a string
+  // http://nodejs.org/api/fs.html#fs_fs_readfilesync_filename_options
+  //response.send(fs.readFileSync('./index.html', 'utf8'));
+  // return the file contents as a buffer, convert it to a string
+  // http://nodejs.org/api/buffer.html#buffer_buf_tostring_encoding_start_end
+  // http://docs.nodejitsu.com/articles/advanced/buffers/how-to-use-buffers
+  response.send(fs.readFileSync('./index.html').toString());
+});
 
 // "static" is called "public" by tj
-app.use(express.static(__dirname + '/static'));
+//app.use(express.static(__dirname + '/static'));
 
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
